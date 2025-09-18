1bb71800cf741fe7d34fea7e4dd1ef32a65cb405
cleverntine <princessclever78@gmail.com>
2025-09-18 08:37:29 +0300

    Add files via upload

---
+++ 20250917-231928.c
@@ -0,0 +1,27 @@
+ //simple c program 
+/*
+name;Cleverntaine mwende 
+registration CT100/G/26234/25
+description:User details 
+*/
+#include<stdio.h>//pre-processor descriptive.scanf
+
+//main function 
+int main(){
+printf("Hello:\n");
+int height;
+printf("Enter your height:");
+scanf("%d",& height);
+printf("Your height is %d");
+
+int phone_no;
+printf("\nEnter your phone_no:");
+scanf("%d",& phone_no);
+printf("\nYour phone_no is %d");
+int weight;
+printf("\nEnter your weight:");
+scanf("%d",& weight);
+printf("\nYour weight is %d");
+    return 0;
+    }
+    
